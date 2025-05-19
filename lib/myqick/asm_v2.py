6640aa26340091d02449a6aa625ec243532dc047
Axelisme <jary33333@gmail.com>
2025-05-19 18:08:25 +0800

    remove patch

--- lib/myqick/asm_v2.py
+++ lib/myqick/asm_v2.py
@@ -3169,36 +3169,3 @@ class AveragerProgramV2(AcquireProgramV2):
             self.label(name)
             self.extend_macros(asm)
             self.ret()
-
-
-# Axelisme patch
-# ----------------------------------------
-class RecordTime(Macro):
-    def __init__(self, register_fn: Callable) -> None:
-        self.register_fn = register_fn
-
-    def preprocess(self, prog):
-        self.register_fn(prog.get_max_timestamp(gens=True, ros=False))
-
-    def expand(self, prog):
-        return []
-
-
-class WithMinLength:
-    def __init__(self, prog: AsmV2, length: float = None):
-        self.prog = prog
-        self.length = length
-        self.start_t = 0.0
-
-    def set_start_time(self, t):
-        self.start_t = t
-
-    def __enter__(self):
-        self.prog.append_macro(RecordTime(self.set_start_time))
-
-    def __exit__(self, exec_type, exec_value, exec_tb):
-        if self.length is not None:
-            self.prog.delay(t=self.start_t + self.length)
-
-
-# ----------------------------------------
